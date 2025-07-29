ec5a695ede764fa4e757064a48407d4ba0bedcf8
Wilson Capacho <wiston.98@hotmail.com>
2025-07-28 21:24:57 -0500

    ajuste en firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDl_MOrto54l6kCzzERO4aXkszgdv20fy4",
-  authDomain: "firechat-2025-9b5c8.firebaseapp.com",
-  projectId: "firechat-2025-9b5c8",
-  storageBucket: "firechat-2025-9b5c8.firebasestorage.app",
-  messagingSenderId: "861131745935",
-  appId: "1:861131745935:web:b0cdc65552991af47d9efa"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
