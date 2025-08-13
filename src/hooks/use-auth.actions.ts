78ec094186a11b1bb7237f7cba68e7d0882a3625
wilson capacho <wiston.98@hotmail.com>
2025-08-12 19:29:38 -0500

    ajuste para borrar datos de cache

--- src/hooks/use-auth.actions.ts
+++ src/hooks/use-auth.actions.ts
@@ -97,6 +97,8 @@ export const useAuthActions = () =>{
         setLoading(true)
         try {
             await signOut(auth);
+
+            window.location.href = "/auth/login"
             return {
                 success: true,
                 error: null,
