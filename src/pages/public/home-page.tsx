7d54dc70aa8d7ce9a240476e34b69dcaa1d5a671
wilson capacho <wiston.98@hotmail.com>
2025-08-13 18:31:48 -0500

    ajustes en el homepage

--- src/pages/public/home-page.tsx
+++ src/pages/public/home-page.tsx
@@ -1,6 +1,10 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div >
+      <h1 className="text-9xl text-blue-700 text-center">
+      HomePage
+      </h1>
+    </div>
   )
 }
 export default HomePage
